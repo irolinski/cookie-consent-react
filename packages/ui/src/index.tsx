462fd85fcd95d0a45ab31daf03d2d7e0d42c2715
irolinski <ignacy.rolinski@gmail.com>
2026-05-24 00:19:35 +0200

    fix: export useful types

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,37 @@
 import { CookieConsent } from "./CookieConsent";
+import type {
+  CookieCategoryType,
+  CookieConsentObject,
+  CookieTagObject,
+  CookieSnippetObject,
+  CookieHandlerFunctionObject,
+  CookieCategorySettings,
+  CookieConsentMode,
+  ColorsType,
+  CategoriesListStyleType,
+  CustomCookieConsentHandlerType,
+  CookieConsentProps,
+  CookieConsentModalProps,
+  CookieConsentTranslationObject,
+  CookieConsentCustomTranslations,
+  CookieConsentAvailableLanguage,
+} from "./types";
 
 export { CookieConsent };
+export type {
+  CookieCategoryType,
+  CookieConsentObject,
+  CookieTagObject,
+  CookieSnippetObject,
+  CookieHandlerFunctionObject,
+  CookieCategorySettings,
+  CookieConsentMode,
+  ColorsType,
+  CategoriesListStyleType,
+  CustomCookieConsentHandlerType,
+  CookieConsentProps,
+  CookieConsentModalProps,
+  CookieConsentTranslationObject,
+  CookieConsentCustomTranslations,
+  CookieConsentAvailableLanguage,
+};
